7703d289ef07c3782fc33a1e6f850b5769776550
Neha <you@example.com>
2024-01-04 15:37:23 +0530

    fiels WIP

--- components/login/index.js
+++ components/login/index.js
@@ -2,22 +2,68 @@ import React from "react";
 import HeadPage from "../Layout/headPage";
 import HEAD_TITLES from "@/utils/constants/titleConstants";
 import styles from "./styles.module.css";
-import { Button, Form } from "react-bootstrap";
-import { FaLock } from "react-icons/fa";
+import { Button, Form, Spinner } from "react-bootstrap";
+//import { FaLock } from "react-icons/fa";
 import { FaArrowLeft , FaArrowRight} from "react-icons/fa6";
 import Link from "next/link";
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import { Field, Formik } from "formik";
+import * as yup from "yup";
+import InputField from "../fields/inputField";
+
+const defaultValues = {
+  email: "",
+  password: "",
+};
 
  
 
 function Login() {
+
+  const handleFormSubmit = async (values) => {
+    await axios
+      .post("https://reqres.in/api/login", {
+        email: values.email,
+        password: values.password,
+      })
+      .then(function (response) {
+        localStorage.setItem("userAuthToken", response.data.token);
+        window.location = "/users";
+      })
+      .catch(function (error) {
+        if (error?.response?.status == 400) {
+          alert(error.response?.data?.error);
+        } else {
+          alert(error.message);
+        }
+      });
+  };
+
+  const validationSchema = yup.object().shape({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6).max(20),
+    // terms: yup.bool().required().oneOf([true], "Terms must be accepted"),
+  });
   return (
     <>
+    
       <HeadPage title={HEAD_TITLES.login}>
         <section>
           <div className="container my-3 pt-5">
             <div className="row d-flex justify-content-center mt-3">
               <div className="col-lg-5 col-md-7 col-12">
-                <Form noValidate onSubmit={() => console.log("====submit==")}>
+               
+              <Formik
+                  validationSchema={validationSchema}
+                  onSubmit={handleFormSubmit}
+                  initialValues={defaultValues}
+                >
+                  {({ handleSubmit, isSubmitting }) => {
+                    return (
+                      <Form noValidate onSubmit={handleSubmit}>
+
                   <div
                     className={`${styles.contactFormWrapper} bg-primary bg-opacity-10 p-md-5 p-2 pt-4 rounded`}
                   >
@@ -40,51 +86,54 @@ function Login() {
                           vehicula, turpis elit. Sed placerat.
                         </p>
                       </div>
-                      <div className="col-12 my-2">
-                        <Form.Control
-                          type="text"
-                          name="email"
-                          className={`form-control ${styles.inputFormControl} shadow-none`}
-                          placeholder="Email"
-                          aria-label="Email"
-                          // value={values.firstName}
-                          // onChange={handleChange}
-                          // isValid={touched.firstName && !errors.firstName}
-                        />
-                        <Form.Control.Feedback>
-                          Looks good!
-                        </Form.Control.Feedback>
-                      </div>
-                      <div className="col-12 my-2">
-                        <Form.Control
-                          type="password"
-                          name="password"
-                          className={`form-control ${styles.inputFormControl} shadow-none`}
-                          placeholder="Password"
-                          aria-label="Password"
-                          // value={values.firstName}
-                          // onChange={handleChange}
-                          // isValid={touched.firstName && !errors.firstName}
-                        />
-                        <Form.Control.Feedback>
-                          Looks good!
-                        </Form.Control.Feedback>
-                      </div>
+
+                        <div className="col-12 my-2">
+                              <Field
+                                name="email"
+                                placeholder="Email Address"
+                                label="Email"
+                                component={InputField}
+                              />
+                            </div>
+                            <div className="col-12 my-2">
+                              <Field
+                                type="password"
+                                name="password"
+                                label="Password"
+                                placeholder="Enter your password"
+                                component={InputField}
+                              />
+                            </div>
 
                       <div className="col-12 mx-auto my-2 mt-4">
                         <Button
                           type="submit"
-                          className="btn w-100 bg-primary bg-opacity-50 py-3 text-white me-2 mb-4 mb-sm-0"
-                       > <FaLock />
+                          className={`btn w-100  ${isSubmitting ? "bg-secondary" : "bg-primary"} bg-opacity-50 py-3 text-white me-2 mb-4 mb-sm-0`}
+
+                       > 
+                       {/* {isSubmitting ? "Submitting..." : "Login >>"} */}
+                       
+                       {isSubmitting ? (
+                                  <Spinner animation="border" role="status">
+                                    <span className="visually-hidden">
+                                      Loading...
+                                    </span>
+                                  </Spinner>
+                                ) : (
+                       
+                                // <FaLock />
                           
-                           Login
+                               Login
+
+                                   )
+                         }
                            
                         </Button>
                       </div>
 
                       <div className="col-12 mt-4 d-flex justify-content-between">
                         <Link href="/./home/">
-                        <FaArrowLeft />back
+                        <FaArrowLeft />  back
                         </Link>
                         <Link href="/./singUp">
                           {" "}
@@ -94,11 +143,16 @@ function Login() {
                     </div>
                   </div>
                 </Form>
+                    );
+                  }}
+                  </Formik>
               </div>
             </div>
           </div>
         </section>
       </HeadPage>
+
+      <ToastContainer />
     </>
   );
 }
